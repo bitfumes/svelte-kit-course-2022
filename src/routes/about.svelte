a56eeef10bf3cae5a36fb08645e967ccc7395fd0
sarthaksavvy <sarthak@bitfumes.com>
2022-07-03 21:58:16 +0530

    04 SvelteKit - first route page

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
+
+<h1>About Page.</h1>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
+
+<h1>Welcome to SvelteKit Bitfumes</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
