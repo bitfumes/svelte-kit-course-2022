83bdac48ea2b0d085d1ea2f73884447a6519f329
sarthaksavvy <sarthak@bitfumes.com>
2022-07-04 09:26:58 +0530

    11 SvelteKit - anchor options

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,5 @@
 <nav>
-	<a href="/">Home</a>
-	<a href="/recipes">Recipes</a>
-	<a href="/about">About</a>
+	<a sveltekit:noscroll href="/">Home</a>
+	<a sveltekit:prefetch href="/recipes">Recipes</a>
+	<a sveltekit:reload href="/about">About</a>
 </nav>
